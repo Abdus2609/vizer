e342ed99a81971acf80270ab7d1d847bf8775f21
Abdus2609 <abdussamad2609@gmail.com>
2024-02-05 12:35:57 +0000

    Initial skeleton files

---
+++ backend/vizor/src/main/java/ic/doc/aah120/App.java
@@ -0,0 +1,11 @@
+package ic.doc.aah120;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
